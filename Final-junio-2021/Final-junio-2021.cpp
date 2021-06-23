d31b68ab9003431f5b6dc0beb582558509f61801
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-22 23:11:33 -0300

    InsertarNodo + aplicacion de funcion

--- Final-junio-2021/Final-junio-2021.cpp
+++ Final-junio-2021/Final-junio-2021.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct InfoLista
 {
     int c1;
-    string c2;
+    char c2;
     float c3;
     int c4;
 
@@ -23,21 +23,19 @@ struct InfoSubLista
     float campo3;
     int campo4;
 
-    InfoSubLista(float dato1, int dato2) {
-        campo3 = dato1;
-        campo4 = dato2;
-    }
+
 };
 
 struct NodoSubLista
 {
     InfoSubLista infSub;
     NodoSubLista* sig;
+
 };
 
 struct InfoResult {
     int c1;
-    string c2;
+    char c2;
     NodoSubLista* subLista;
 };
 
@@ -64,13 +62,56 @@ InfoLista Pop(NodoLista* &lista) {
     return res;
 }
 
+void InsertarSubNodo(NodoSubLista* lista, NodoSubLista* insert ) {
+    NodoSubLista* aux = lista;
+    while (aux->sig != NULL) {
+        aux = aux->sig;
+    }
+    aux->sig = insert;
+}
+
+NodoListaResult* CargarListaDeListas(NodoLista* &lista) {
+    NodoListaResult* nr = new NodoListaResult();
+    NodoListaResult* nraux = new NodoListaResult();
+    bool primerNodo = true;
+    while (lista->sig != NULL) {
+        InfoLista il = Pop(lista);
+        if (primerNodo) {
+            nr->info.c1 = il.c1;
+            nr->info.c2 = il.c2;
+            NodoSubLista* nsl = new NodoSubLista();
+            nsl->infSub.campo3 = il.c3;
+            nsl->infSub.campo4 = il.c4;
+            nr->info.subLista = nsl;
+            NodoListaResult* nraux = nr;
+        }
+        else {
+            while ( il.c1 == nraux->info.c1 && il.c2 == nraux->info.c2) {
+                NodoSubLista* ns = new NodoSubLista();
+                InfoSubLista subInfo;
+                subInfo.campo3 = il.c3;
+                subInfo.campo4 = il.c4;
+                ns->infSub = subInfo;
+                ns->sig = NULL;
+
+                InsertarSubNodo(nr->info.subLista, ns);
+                nraux = nraux->sig;
+            }
+        }
+        
+    }
+
+
+    return nr;
+}
+
 
 int main()
 {
     NodoLista* nl = new NodoLista();
     InfoLista* il = new InfoLista();
     il->c1 = 2;
-    il->c2 = "string";
+    il->c2 = 'C';
     il->c3 = 2;
     il->c4 = 32;
 
